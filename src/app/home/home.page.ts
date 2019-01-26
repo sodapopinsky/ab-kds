fd0c7abbcf882f2d561311428d1501ea10b01b7e
Nick Spitale <nick@theatomicburger.com>
2019-01-26 11:19:48 -0600

    nts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Order } from 'ab-shared/models';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
+  orders: Order;
+  constructor(){
+    
+  }
+  
 }
